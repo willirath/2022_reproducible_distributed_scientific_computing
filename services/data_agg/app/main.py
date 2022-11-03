24de258674cd381291ee9ae893c54446ae281f5a
Willi Rath <wrath@geomar.de>
2022-11-03 11:09:03 +0100

    Add data aggregation service

---
+++ services/data_agg/app/main.py
@@ -0,0 +1,71 @@
+from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+
+import pandas as pd
+import numpy as np
+import asciichartpy
+
+import requests
+
+app = FastAPI()
+
+
+class DataAgg(object):
+    def __init__(self, num_particles: int = 5):
+        """Initialize with number of particles."""
+        self.num_particles = num_particles
+        self.diags_init()
+        for _ in range(30):
+            self.diags_run()
+
+    def diags_init(self):
+        self.diags = pd.DataFrame()
+
+    def diags_run(self):
+        positions = self.query_particle_positions()
+        x = np.array([p[0] for p in positions])
+        y = np.array([p[1] for p in positions])
+        com_x = x.mean()
+        com_y = y.mean()
+        moi = x.var() + y.var()
+        self.diags = pd.concat(
+            [
+                self.diags,
+                pd.DataFrame({"com_x": [com_x,], "com_y": [com_y], "moi": [moi,]}),
+            ],
+            ignore_index=True,
+        )
+        self.diags = self.diags.tail(35).reset_index(drop=True)
+
+    def diags_show(self):
+        self.diags_run()
+        com_x_chart = "center of mass X:\n" + asciichartpy.plot(
+            self.diags["com_x"], {"height": 5}
+        )
+        com_y_chart = "center of mass Y:\n" + asciichartpy.plot(
+            self.diags["com_y"], {"height": 5}
+        )
+        moi_chart = "moment of inertia:\n" + asciichartpy.plot(
+            self.diags["moi"], {"height": 5}
+        )
+        return "\n\n".join(
+            ["== Random Walk Stats ==", com_x_chart, com_y_chart, moi_chart]
+        )
+
+    def query_particle_positions(self):
+        positions = []
+        for n in range(self.num_particles):
+            _ = requests.get(f"http://services_particles_{n+1:d}:80/step/")
+            resp = requests.get(f"http://services_particles_{n+1:d}:80/pos/")
+            print(resp.json())
+            positions.append(resp.json())
+        return positions
+
+
+app.dataagg = DataAgg()
+
+
+@app.get("/diags_show/", response_class=PlainTextResponse)
+async def diags_show():
+    """Show diags"""
+    return app.dataagg.diags_show()
