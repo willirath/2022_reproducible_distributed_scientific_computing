f386e0af3bf6916cb828cdc804dc17d0b4db7d8f
Willi Rath <wrath@geomar.de>
2022-12-12 16:50:53 +0100

    Clean up

--- services/data_agg/app/main.py
+++ services/data_agg/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
 
 import pandas as pd
+import json
 import numpy as np
 import asciichartpy
 
@@ -11,50 +12,65 @@ app = FastAPI()
 
 
 class DataAgg(object):
-    def __init__(self, target_step=10):
+    def __init__(self, target_step: int = 10):
         """Initialize with number of particles."""
         self.target_step = 10
-        self.redis_store = redis.Redis(host="data_agg_redis", port=6379)
-        self.redis_store.hset("config", "target_step", self.target_step)
+        self.redis_store = redis.Redis(host="redis_store", port=6379)
+        self.set_target_step(target_step)
+
+    def set_target_step(self, target_step):
+        self.redis_store.hset("config", "target_step", target_step)
 
     def diags_run(self):
-        positions = self.query_particle_positions()
-        x = np.array([p[0] for p in positions])
-        y = np.array([p[1] for p in positions])
-        com_x = x.mean()
-        com_y = y.mean()
-        moi = x.var() + y.var()
-        self.diags = pd.concat(
-            [
-                self.diags,
-                pd.DataFrame({"com_x": [com_x,], "com_y": [com_y], "moi": [moi,]}),
-            ],
-            ignore_index=True,
-        )
-        self.diags = self.diags.tail(25).reset_index(drop=True)
-
-    def data_show(self):
         df = self.get_particle_positions()
-        return str(df)
 
-    def diags_show(self):
-        df = self.get_particle_positions()
         com_x = df.groupby("t")["x"].mean()
+        com_y = df.groupby("t")["y"].mean()
+        moi = df.groupby("t")["x"].var() + df.groupby("t")["y"].var()
+        count = df.groupby("t")["x"].count()
+
+        return com_x, com_y, moi, count
+
+    def data_str(self):
+        """Get (shortened) string representation of the data."""
+        return str(self.get_particle_positions())
+
+    def data_json(self):
+        """Get JSON version of all data."""
+        return json.loads(self.get_particle_positions().to_json())
+
+    def charts(self, rolling_length: int = None):
+        """Produce ASCII charts of center of mass and of moment of inertia."""
+        com_x, com_y, moi, count = self.diags_run()
+        if rolling_length is not None:
+            com_x = com_x.tail(rolling_length)
+            com_y = com_y.tail(rolling_length)
+            moi = moi.tail(rolling_length)
+
         com_x_chart = "center of mass X:\n" + asciichartpy.plot(
             list(com_x), {"height": 5}
         )
-        com_y = df.groupby("t")["y"].mean()
         com_y_chart = "center of mass Y:\n" + asciichartpy.plot(
             list(com_y), {"height": 5}
         )
-        moi = df.groupby("t")["x"].var() + df.groupby("t")["y"].var()
-        print(moi)
         moi_chart = "moment of inertia:\n" + asciichartpy.plot(list(moi), {"height": 5})
+
+        count_chart = "number of data points:\n" + asciichartpy.plot(
+            list(count), {"height": 3}
+        )
+
         return "\n\n".join(
-            ["== Random Walk Stats ==", com_x_chart, com_y_chart, moi_chart]
+            [
+                "== Random Walk Stats ==",
+                com_x_chart,
+                com_y_chart,
+                moi_chart,
+                count_chart,
+            ]
         )
 
     def get_particle_positions(self):
+        """Build particle position DataFrame from REDIS contents."""
         particle_ids = list(
             map(lambda b: b.decode("utf-8"), self.redis_store.smembers("particle_ids"))
         )
@@ -72,13 +88,25 @@ class DataAgg(object):
 app.dataagg = DataAgg()
 
 
-@app.get("/data_show/", response_class=PlainTextResponse)
-async def data_show():
-    """Show diags"""
-    return app.dataagg.data_show()
+@app.get("/data/str", response_class=PlainTextResponse)
+async def data_str():
+    """Get data as STR."""
+    return app.dataagg.data_str()
 
 
-@app.get("/diags_show/", response_class=PlainTextResponse)
-async def diags_show():
-    """Show diags"""
-    return app.dataagg.diags_show()
+@app.get("/data/json")
+async def data_str():
+    """Get data as JSON."""
+    return app.dataagg.data_json()
+
+
+@app.get("/charts/", response_class=PlainTextResponse)
+async def charts(rolling_length: int = 100):
+    """Show charts."""
+    return app.dataagg.charts(rolling_length=rolling_length)
+
+
+@app.get("/set_target_step/", response_class=PlainTextResponse)
+async def set_target_step(target_step: int = 10):
+    app.dataagg.set_target_step(target_step)
+    return f"Set target step to {target_step}."
--- services/particles/app/particle.py
+++ services/particles/app/particle.py
@@ -5,61 +5,58 @@ import time
 
 
 class Particle(object):
+    main_maxiter = 10_000
+    stepsize = 1.0
+
     def __init__(self, x: int = 0, y: int = 0, t: int = 0):
-        self.x = x
-        self.y = y
-        self.t = t
-        self.main_maxiter = 1_000
+        self.x, self.y, self.t = x, y, t
+        self.redis_setup()
         self.lgc_params = requests.get(
             "http://rng_loadbalancer:4000/lgc_params/"
         ).json()
-        self.redis_setup()
-        self.main_loop()
+        self.run_main_loop()
 
-    def main_loop(self):
+    def redis_setup(self):
+        """Draw unique particle ID and register it in REDIS store."""
+        self.particle_id = str(uuid.uuid4())
+        self.redis = redis.Redis(host="redis_store", port=6379)
+        self.redis.sadd("particle_ids", self.particle_id)
+
+    def get_rand(self, x: int = None):
+        """Get random number from RNG service and scale to [-1, 1)."""
+        resp = requests.get("http://rng_loadbalancer:4000/lgc/", params={"x": x})
+        N = int(resp.json())
+        return 2 * N / self.lgc_params["m"] - 1
+
+    def step(self):
+        """Move by random step and increment time step."""
+        self.x += self.get_rand() * self.stepsize
+        self.y += self.get_rand() * self.stepsize
+        self.t += 1
+
+    def store_position(self):
+        """Save position to REDIS store."""
+        self.redis.rpush(f"{self.particle_id}:x", self.x)
+        self.redis.rpush(f"{self.particle_id}:y", self.y)
+        self.redis.rpush(f"{self.particle_id}:t", self.t)
+
+    def run_main_loop(self):
+        """Poll for work (target_steps from REDIS store) an step / store pos."""
         self.main_niter = 0
         while self.main_maxiter > self.main_niter:
             self.main_niter += 1
-            time.sleep(0.5)  # sleep before checking for work again
-            target_step = self.redis_store.hget("config", "target_step")
-            if target_step is None:
-                continue
-            self.target_step = int(target_step)
-            while self.t < self.target_step:
+
+            # get target num of time steps
+            target_step = int(self.redis.hget("config", "target_step") or 0)
+            # target_step = int(target_step)
+
+            # add steps if necessary
+            while self.t < target_step:
                 self.step()
+                self.store_position()
 
-    def redis_setup(self):
-        self.particle_id = str(uuid.uuid4())
-        self.redis_store = redis.Redis(host="data_agg_redis", port=6379)
-        self.redis_store.sadd("particle_ids", self.particle_id)
+            time.sleep(0.5)  # wait before checking for more work
 
-    def query_rng(self, x: int = None):
-        resp = requests.get("http://rng_loadbalancer:4000/lgc/", params={"x": x})
-        return int(resp.json())
 
-    def normal_rand(self, num_summed: int = 11):
-        random_ints = (self.query_rng() for n in range(num_summed))
-        normal = (sum(random_ints) / self.lgc_params["m"] - num_summed / 2) / (
-            num_summed / 12
-        ) ** 0.5
-        return normal
-
-    def step(self):
-        self.x += 2 * self.query_rng() / self.lgc_params["m"] - 1.0
-        self.y += 2 * self.query_rng() / self.lgc_params["m"] - 1.0
-        self.t += 1
-        self.position_store()
-
-    def position_store(self):
-        self.redis_store.rpush(
-            f"{self.particle_id}:x", self.x,
-        )
-        self.redis_store.rpush(
-            f"{self.particle_id}:y", self.y,
-        )
-        self.redis_store.rpush(
-            f"{self.particle_id}:t", self.t,
-        )
-
-    def position(self):
-        return (self.x, self.y, self.t)
+if __name__ == "__main__":
+    particle = Particle()
